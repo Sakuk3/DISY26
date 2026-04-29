49c9cd381cc0a5148dffdf418e1690df4dcb88d0
Lukas Fritsch <lukas.fritsch@protonmail.com>
2026-04-29 22:44:51 +0200

    Adapt dtos to use new java date api

--- common/src/main/java/at/uastw/disys26bwi/common/dto/HistoricEnergyDto.java
+++ common/src/main/java/at/uastw/disys26bwi/common/dto/HistoricEnergyDto.java
@@ -1,5 +1,6 @@
 package at.uastw.disys26bwi.common.dto;
 
-import java.util.Date;
 
-public record HistoricEnergyDto(Date startDate, Date endDate, double communityProduced, double communityUsed, double gridUsed) {}
+import java.time.LocalDateTime;
+
+public record HistoricEnergyDto(LocalDateTime startDate, LocalDateTime endDate, double communityProduced, double communityUsed, double gridUsed) {}
--- ui/src/main/java/at/uastw/disys26bwi/ui/EnergyService.java
+++ ui/src/main/java/at/uastw/disys26bwi/ui/EnergyService.java
@@ -3,7 +3,7 @@ package at.uastw.disys26bwi.ui;
 import at.uastw.disys26bwi.common.dto.CurrentEnergyDto;
 import at.uastw.disys26bwi.common.dto.HistoricEnergyDto;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.Random;
 
 public class EnergyService {
@@ -11,23 +11,23 @@ public class EnergyService {
   
   public CurrentEnergyDto getCurrentEnergy() {
     return new CurrentEnergyDto(
-        new Date(),
-        random.nextDouble(),
-        random.nextDouble()
+      LocalDateTime.now(),
+      random.nextDouble(),
+      random.nextDouble()
     );
   }
   
-  public HistoricEnergyDto getHistoricEnergy(Date start, Date end) {
+  public HistoricEnergyDto getHistoricEnergy(LocalDateTime start, LocalDateTime end) {
     double communityProduced = 1000 + random.nextDouble() * 9000;
     double gridUsed = random.nextDouble() * communityProduced;
     double communityUsed = Math.max(0, communityProduced - gridUsed);
     
     return new HistoricEnergyDto(
-        start,
-        end,
-        communityProduced,
-        communityUsed,
-        gridUsed
+      start,
+      end,
+      communityProduced,
+      communityUsed,
+      gridUsed
     );
   }
 }
