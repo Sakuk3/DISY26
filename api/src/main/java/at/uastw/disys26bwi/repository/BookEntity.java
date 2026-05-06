c266cfff6b65f09012f73a38ae70c22c2bdfe34f
Lukas Fritsch <lukas.fritsch@protonmail.com>
2026-05-06 20:49:01 +0200

    fix book db sql

--- api/src/main/java/at/uastw/disys26bwi/repository/BookEntity.java
+++ api/src/main/java/at/uastw/disys26bwi/repository/BookEntity.java
@@ -11,7 +11,6 @@ public class BookEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
-  @Column(name = "name")
   private String title;
   private String genre;
 
